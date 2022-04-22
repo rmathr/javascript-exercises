38cfdd3d4df2080ff4ba2f7b955dc44d1db5a214
Matheus <matrodrigues@live.com>
2022-04-22 17:59:21 -0300

    End exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.match(/[^_\W]+/g).join('')
+         .toLowerCase();
+    const palindromeString = reverseString(newString);
+    if(palindromeString == newString){
+        return true;
+    } else {
+        return false;
+    }
 };
+
+const reverseString = function(string) {
+    const reversedString = [];
+    for (i = 0; i<string.length; i++){
+        reversedString.unshift(string[i]);
+    }
+    return reversedString.join("");
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
