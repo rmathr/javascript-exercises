83009c363c227f52a65052434b44032dd1d7f578
Matheus <matrodrigues@live.com>
2022-04-22 20:13:11 -0300

    End exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fibonacciSeries = [1,1];
+    num = +num;
+    if(num < 0){
+        return "OOPS";
+    } else if(num == 0) {
+        return fibonacciSeries[0];        
+    } else if (num == 1){
+        return fibonacciSeries[1];
+    } else {
+        for (let i = 1; i < num; i++){
+            fibonacciSeries.push(fibonacciSeries[i-1]+fibonacciSeries[i]);
+        }
+    } return fibonacciSeries[num-1];  
 };
 
 // Do not edit below this line
