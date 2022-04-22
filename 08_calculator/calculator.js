31818cbb54ea7d4e1a4423bdb8e3826537a7ca64
Matheus <matrodrigues@live.com>
2022-04-22 17:20:00 -0300

    End exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,65 @@
-const add = function() {
-	
+const add = function(x,y) {
+	if(typeof x != 'number' || typeof y != 'number'){
+    return "ERROR";
+  } else {
+    return x+y;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	if(typeof x != 'number' || typeof y != 'number'){
+    return "ERROR";
+  } else {
+  return x - y;
+}
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (!(Array.isArray(arr))){
+    return "ERROR";
+  } else {
+    const sumResult = arr.reduce((total, num) => {
+      return total + num;
+    }, 0);
+    return sumResult;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (!(Array.isArray(arr))){
+    return "ERROR";
+  } else {
+    const multiplyResult = arr.reduce((total, num) => {
+      return total*num;
+    }, 1);
+    return multiplyResult;
+  }
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	if(typeof x != 'number' || typeof y != 'number'){
+    return "ERROR";
+  } else {
+    let powerResult = x;
+    for (let i = 0; i < y-1; i++){
+      powerResult *= x;
+    }
+    return powerResult;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(typeof x != 'number' || x < 0){
+    return "ERROR";
+  } else if (x == 0 || x == 1) {
+    return 1;
+    } else {
+      let factorialResult = x;
+      for(let i = 1; i < x; i++){
+        factorialResult *= (x - i);
+      }
+      return factorialResult;
+    }
 };
 
 // Do not edit below this line
