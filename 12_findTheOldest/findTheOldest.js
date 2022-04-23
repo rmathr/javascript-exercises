403d553cb4455821bee20e6d97c0f5b7b3de9c2c
Matheus <matrodrigues@live.com>
2022-04-23 10:17:54 -0300

    End exercise 12-find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    const oldest = obj.sort((a,b) => {
+        if (a.yearOfDeath === undefined){
+            const lastGuy = new Date().getFullYear() - a.yearOfBirth;
+            const nextGuy = b.yearOfDeath - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+        } else if (b.yearOfDeath === undefined){
+            const lastGuy = a.yearOfDeath - a.yearOfBirth;
+            const nextGuy = new Date().getFullYear() - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+        } else {
+            const lastGuy = a.yearOfDeath - a.yearOfBirth;
+            const nextGuy = b.yearOfDeath - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+        }   
+      });
+      return oldest[0];
 };
 
 // Do not edit below this line
